fc6efb0570305520f2a73b5a63a1df5d22bdf76b
Cwilson-WhisperingRock <xpropellorheadx@gmail.com>
2022-01-26 10:49:00 -0800

    Added outline

--- I2C_Practice/I2C_Practice.ino
+++ I2C_Practice/I2C_Practice.ino
@@ -1,9 +1,35 @@
-void setup() {
-  // put your setup code here, to run once:
+// Purpose : Communicate with the raspberry pi as a sub device
+// Context : Pi will give the arduino a number and the Duino will repond with bool if its divisable by 2
 
+#include <Wire.h>
+
+// Arduino Addresses
+#define PI_ADD 0x01
+#define ARD_ADD_1 0x7                 // This device
+#define ARD_ADD_2 0x14
+#define ARD_ADD_3 0x28
+
+bool EVEN = false;
+
+void setup() {
+  Wire.begin(ARD_ADD_1);              // ARD device joins I2C bus as sub with address
+  Serial.begin(9600);                 // Serial for user
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+Wire.beginTransmission(44); // transmit to device #44 (0x2c)
+                              // device address is specified in datasheet
+  Wire.write(val);             // sends value byte  
+  Wire.endTransmission();     // stop transmitting
+
+  val++;        // increment value
+  if(val == 64) // if reached 64th position (max)
+  {
+    val = 0;    // start over from lowest value
+  }
+  delay(500);
 }
+
+
+void I2C_TX
