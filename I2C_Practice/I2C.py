1dd799642682429c4be5b065a05532f38aa5d18d
CWilson-WhisperingRock <xpropellorheadx@gmail.com>
2022-01-26 16:14:57 -0800

    Unwritten & Untested

---
+++ I2C_Practice/I2C.py
@@ -0,0 +1,29 @@
+
+# Purpose : Communicate with the arduino as a main device
+# Context : Pi will give Ard a number, and output its response to serial
+
+
+# Serial input
+
+# I2C Addresses
+PI_ADD = 0x01
+ARD_ADD_1 = 0x14
+ARD_ADD_2 = 0x28
+ARD_ADD_3 = 0x42
+
+# Data Handling Variables
+userData = 0
+userDataByte = 1
+bool RXData = False
+i2c_error = 0
+
+
+
+
+
+
+
+
+
+    
+
